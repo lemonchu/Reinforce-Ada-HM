825f152219c7c543bbc56cf10de598091b384724
lemonchu <lemonpig-chu@qq.com>
2026-06-16 20:36:13 -0400

    add reweight

---
+++ scripts/slurm/fix_bias_and_reweight_1.5B.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=mengchu_harmonic_eval_orz_b
+#SBATCH --output=mengchu_b.out
+#SBATCH --error=mengchu_b.err
+#SBATCH --partition=preempt
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128G
+#SBATCH --time=48:00:00
+#SBATCH --gres=gpu:RTX_PRO_6000:4
+
+cd /data/group_data/cx_group/lemonpig/Reinforce-Ada
+source .venv/bin/activate
+bash scripts/run_reinforce_ada_fix_reweight.sh
+sleep 300
